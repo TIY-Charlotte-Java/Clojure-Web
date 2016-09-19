fc281cdfc60326105a6cdd2dbb5bad8806cc97fd
michael fahrner <Mike Fahrner>
2016-09-19 12:54:08 -0400

    web works and sorts by id and category

--- src/clojure_web/core.clj
+++ src/clojure_web/core.clj
@@ -1,6 +1,39 @@
-(ns clojure-web.core)
+(ns clojure-web.core
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-purchases []
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                      (str/split line #","))
+                    purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                      (zipmap header line))
+                    purchases)]
+    purchases))
+
+(defn purchases-html []
+  (let [purchases (read-purchases)]
+    [:ol
+     (map (fn [purchases]
+            [:li (str (get purchases "customer_id") " " (get purchases "category"))])
+          purchases)]))
+
+(c/defroutes app
+             (c/GET "/:category{.*}" [category]
+                    (h/html [:html
+                             [:body
+                              (purchases-html)]])))
+
+(defonce server (atom nil))
+
+(defn -main []
+  (when @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
