19a6c35e6e6206e01a8acc7f0fea78e8b9ac0366
wrodgers95 <will.rodgers1995@gmail.com>
2016-09-21 09:18:53 -0400

    almost finished

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject clojure-web "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :main clojure-web.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [hiccup "1.0.5"]
+                 [compojure "1.5.0"]]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :aot [clojure-web.core])
+
--- src/clojure_web/core.clj
+++ src/clojure_web/core.clj
@@ -1,6 +1,64 @@
-(ns clojure-web.core)
+(ns clojure-web.core
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce server (atom nil))
+
+(defn purchaseList []
+  (println "What category would you like to filter by...")
+  (let
+    [purchases (slurp "purchases.csv")
+     purchases (str/split-lines purchases)
+     purchases (map (fn [line]
+                      (str/split line #","))
+                    purchases)
+     header (first purchases)
+     purchases (rest purchases)
+     purchases (map (fn [line]
+                      (zipmap header line))
+                    purchases)
+     ;category (read-line)
+     ;purchases (filter (fn [line]
+     ;                    (= (get line "category") category))
+     ;                  purchases)
+     ]
+     purchases))
+
+(defn purchases-html [category]
+  (let [purchases (purchaseList)
+        purchases (if (= 0 (count category))
+              purchases
+              (filter (fn [purchases]
+                      (= (get purchases "category") category))
+                              purchases))]
+     [:ol
+      (map (fn [purchases]
+             [:li (str (get purchases "customer_id") " "
+                       (get purchases "date") " "
+                       (get purchases "credit_card") " "
+                       (get purchases "cvv") " "
+                       (get purchases "category"))])
+           purchases)]))
+
+(c/defroutes app
+             (c/GET "/:category{.*}" [category]
+               (h/html [:html
+                        [:body
+                         [:a {:href "/Alcohol"} "Alcohol "]
+                         [:a {:href "/Furniture"} "Furniture "]
+                         [:a {:href "/Toiletries"} "Toiletries "]
+                         [:a {:href "/Shoes"} "Shoes "]
+                         [:a {:href "/Alcohol"} "Alcohol "]
+                         [:a {:href "/Jewelry"} "Jewelry "]
+                         [:a {:href "/Food"} "Food "]
+                         (purchases-html category)]])))
+
+(defonce server (atom nil))
+
+(defn -main []
+  (when @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3001 :join? false})))
